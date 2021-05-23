2b3f7c650a80b50c0b3d1bdca4314bf102663f73
Selvedin Haskic <Selvedin.Haskic@infobip.com>
2021-05-23 19:08:07 +0200

    adding scaffold to screen

--- lib/screens/finished_tasks_screen.dart
+++ lib/screens/finished_tasks_screen.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class FinishedTasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Finished tasks'),
+      ),
+      body: Center(
+        child: Text('This is the place for finished tasks'),
+      ),
+    );
   }
 }
