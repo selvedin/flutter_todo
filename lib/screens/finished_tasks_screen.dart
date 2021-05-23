7721544e2fda37d2922613ae91b0f4fb29170746
Selvedin Haskic <Selvedin.Haskic@infobip.com>
2021-05-23 20:54:28 +0200

    adding image

--- lib/screens/finished_tasks_screen.dart
+++ lib/screens/finished_tasks_screen.dart
@@ -23,17 +23,27 @@ class FinishedTasksScreen extends StatelessWidget {
         ],
       ),
       body: Container(
-        height: 400,
-        child: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: tasks.length,
-          itemBuilder: (_, index) => TaskItem(
-            id: tasks[index].id,
-            title: tasks[index].title,
-            createdAt: tasks[index].createdAt,
-            isFinished: tasks[index].isFinished,
-          ),
-        ),
+        height: MediaQuery.of(context).size.height,
+        child: tasks.length > 1
+            ? ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: tasks.length,
+                itemBuilder: (_, index) => TaskItem(
+                  id: tasks[index].id,
+                  title: tasks[index].title,
+                  createdAt: tasks[index].createdAt,
+                  isFinished: tasks[index].isFinished,
+                ),
+              )
+            : Container(
+                child: Column(
+                  children: [
+                    Text('No finished tasks'),
+                    Image.network(
+                        'https://icons.iconarchive.com/icons/cornmanthe3rd/squareplex/512/Utilities-tasks-icon.png'),
+                  ],
+                ),
+              ),
       ),
     );
   }
